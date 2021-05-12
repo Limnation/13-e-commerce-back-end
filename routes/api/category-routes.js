5042e711f66b323f0053917b982a5cb5c8a0d994
Scott Erwin <scottallenerwin@gmail.com>
2021-05-12 19:41:09 -0400

    added find all categories async

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,31 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
+  const categoryData = await Category.findAll().catch((err) => {
+    res.status(500).json(err);
+  });
   // be sure to include its associated Products
+  res.status(200).json(categoryData);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
 });
 
