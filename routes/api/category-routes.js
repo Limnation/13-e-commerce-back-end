a2ea282438cdbe9158e14d5117342b05c77f821f
Scott Erwin <scottallenerwin@gmail.com>
2021-05-12 21:29:21 -0400

    added update/put to category rourtes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,6 +48,20 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No Category with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
@@ -63,7 +77,6 @@ router.delete("/:id", (req, res) => {
       res.status(404).json({ message: "No Category found with that id!" });
       return;
     }
-
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
